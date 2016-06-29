71b585ac7459e2c58346c7d111812dd75d27d393
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-29 15:40:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do 
+    erb :super_hero
+  end
 
+  post '/teams' do 
+    #construct data here
+    @team = Team.new(params[:team])
+
+    params[:team][:members].each do |member_details|
+      # binding.pry
+      Hero.new(member_details)
+    end
+
+    @heroes = Hero.all
+
+    erb :team
+  end
 
 end
